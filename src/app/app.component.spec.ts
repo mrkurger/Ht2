6687ba4f0a9907acf9cd11512fc9ed414b281394
mrkurger <84293583+mrkurger@users.noreply.github.com>
2023-07-09 06:41:39 +0200

    Revert "refactor: replace template strings with regular string literals"
    This reverts commit 38bc1d1ca7f455bcc5cac5e9b5fb6f03c6234962.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it("should have as title 'project'", () => {
+  it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('project');
