98aac611acbad2a7061e3386dd7840a01f1650ae
mrkurger <84293583+mrkurger@users.noreply.github.com>
2023-07-09 12:08:15 +0200

    yy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component'; // make sure this path is correct
-import { HomePageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { AdvertismentComponent } from './advertisment/advertisment.component';
 
